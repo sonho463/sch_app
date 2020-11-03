3cfc04408140357bae7b4845b5a03cb1d4fdd46a
sonho463 <son.ho463@gmail.com>
2020-11-03 20:45:41 +0900

    nokogiriやってみたけど

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,18 +3,16 @@ class HomeController < ApplicationController
   require 'open-uri'
   
   def top
-    url = 'https://qiita.com/search?q=ruby'
-    
-    charset = nil
-    
-    html = open(url) do |f|
-        charset = f.charset
-        f.read
-    end
-    
-    doc = Nokogiri::HTML.parse(html, nil, charset)
-    doc.xpath('//h1[@class="searchResult_itemTitle"]').each do |node|
-      @node = node.css('a').inner_text
-    end
+    url = 'http://agora.ex.nii.ac.jp/cps/weather/warning/'
+
+    # タイトルが「大阪府」の情報を取得する
+    # .title.inner_text == "島根県"
+    # //img[contains(@class, 'image')]
+    doc = Nokogiri::HTML(open(url))
+    "User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36")
+    # doc.xpat(//html/body/div[2]/div[1]).each do |title|
+    @title = doc.xpath(//body/div[2]/div[1])
+      
+
   end
 end
